d8213c9319f6e1afa5b662ec644968662164eda7
Abhishek Srujan <srujan.abhishek@gmail.com>
2019-05-19 08:06:39 +0200

    Extend route_planner.h

--- src/route_planner.h
+++ src/route_planner.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef ROUTE_PLANNER_H
+#define ROUTE_PLANNER_H
 
 #include <iostream>
 #include <vector>
@@ -10,8 +11,16 @@ class RoutePlanner {
   public:
     RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
     // Add public variables or methods declarations here.
+    float GetDistance() { return distance;}; // used to print out the total distance from `main.cpp`
 
   private:
     // Add private variables or methods declarations here.
     RouteModel &m_Model;
+    RouteModel::Node* start_node; // Will point to the nodes in the model closest to the input nodes
+    RouteModel::Node* end_node; // Will point to the nodes in the model closest to the input nodes
+    float distance;// Will hold the total distance from the start node to the end node
+  
+  
 };
+
+#endif

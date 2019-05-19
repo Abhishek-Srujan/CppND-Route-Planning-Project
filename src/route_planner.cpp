31fd089fd8462b6157290c3bd7f43ac7905b5304
Abhishek Srujan <srujan.abhishek@gmail.com>
2019-05-19 22:01:54 +0200

    Updated AStarSearch to actually perform the A*Search

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -35,9 +35,19 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
 /* A*Search */
 void RoutePlanner::AStarSearch()
 {
-  end_node -> parent = start_node;
-  m_Model.path = ConstructFinalPath(end_node);
-  return;
+  start_node->visited = true;
+  open_list.push_back(start_node);
+  RouteModel::Node *current_node = nullptr;
+  while (open_list.size() > 0)
+  {
+    current_node = NextNode();
+    if (current_node -> distance(*end_node) == 0)
+    {
+      m_Model.path = ConstructFinalPath(current_node);
+      return;
+    }
+      AddNeighbors(current_node); 
+  }
 }
 
 /* Calculate H Value. The method will return the distance from the passed argument to the `end_node`*/
